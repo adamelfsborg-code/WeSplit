4a943905d0e5940d17e58864f2795f47fde25919
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-06 17:57:51 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adam Elfsborg on 2024-07-06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
